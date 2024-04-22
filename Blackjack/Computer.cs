5b7060a6f5cd551b60fede055890a971ffe05612
joepu <joepu@JCT_XP5>
2024-04-22 14:41:45 +0100

    Add project files.

---
+++ Blackjack/Computer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Blackjack
+{
+    internal class Computer
+    {
+        public bool startGame()
+        {
+
+            bool gameloop = false;
+            bool playerwin = false;
+
+            Deck deck = new Deck();
+            Hand payerHand = new Hand();
+            Hand dealerHand = new Hand();
+
+            payerHand.AddCard(deck.dealACard());
+            dealerHand.AddCard(deck.dealACard());
+            payerHand.AddCard(deck.dealACard());
+            dealerHand.AddCard(deck.dealACard());
+
+            Console.WriteLine($"The Dealers first card is {dealerHand.showdealercard()}");
+
+            while (gameloop == false)
+            {
+                payerHand.showCards();
+                Console.WriteLine("Would you like to Stand (s) or Hit (h)?");
+                string playerResult = Console.ReadLine();
+                if (playerResult == "s")
+                {
+                    gameloop = true;
+                    Console.WriteLine("You have ended your turn.");
+                }
+                else if (playerResult == "h")
+                {
+                    payerHand.AddCard(deck.dealACard());
+                    if (payerHand.bust())
+                    {
+                        gameloop = true;
+                        Console.WriteLine("You have gone bust.");
+                    }
+                }
+            }
+            Console.WriteLine($"Your current total is: {payerHand.GetValue()}");
+            Console.WriteLine("Dealer total: ");
+            Console.WriteLine(dealerHand.GetValue());
+
+            while (dealerHand.GetValue() <= 16)
+            {
+                dealerHand.AddCard(deck.dealACard());
+            }
+            if (dealerHand.GetValue() >= 21)
+            {
+                Console.WriteLine("The dealer has gone bust");
+            }
+
+            if (dealerHand.GetValue() < payerHand.GetValue() && payerHand.bust() == false)
+            {
+                playerwin = true;
+            }
+
+            return playerwin;
+        }
+
+
+    }
+}
---
+++ Blackjack/Program.cs
@@ -0,0 +1,72 @@
+﻿using Blackjack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Computer computer = new Computer();            
+            bool gamerounds = true;
+            int bet = 0;
+            int Credit = 1000;
+            Console.WriteLine("Welcome to Gamble your money AWAY BlackJack");
+            Console.WriteLine($"Your Credit is £{Credit}");
+
+            while (gamerounds)
+            {
+                Console.WriteLine($"How much would you like to bid out of your £{Credit}");
+                try
+                {
+                    bet = int.Parse(Console.ReadLine());
+                    if (bet > Credit)
+                    {
+                        Console.WriteLine($"You are betting over your full credit amount, setting bid to £{Credit}");
+                        bet = Credit;
+                    }
+                    else
+                    {
+                        Credit = Credit - bet;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not a number -  I'm setting your bet to your whole balance");
+                    bet = Credit;
+                    Credit -= bet;
+                }
+
+                Console.WriteLine($"Your bet is {bet}");
+
+                if (computer.startGame())
+                {
+                    Console.WriteLine("You have won.");
+                    Credit = Credit + (bet * 2);
+                }
+                else
+                {
+                    Console.WriteLine("You have lost.");
+                }
+                Console.WriteLine($"Your new credit is £{Credit}");
+                Console.WriteLine("Play again, you could win your money back (y/n)");
+                if (Console.ReadLine() == "n")
+                {
+                    gamerounds = false;
+                }
+            }
+
+            Console.WriteLine("The addiction only just started...");
+            Console.WriteLine($"Your end Credit was £{Credit}");
+            Console.ReadLine();
+        }
+    }
+}
+
